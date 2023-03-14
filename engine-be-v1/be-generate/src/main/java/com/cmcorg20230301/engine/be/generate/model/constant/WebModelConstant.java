49d36351fd43f16dad073a4c4296486e992827d3
法外狂徒李四 <dimensional_logic@qq.com>
2023-03-15 00:24:18 +0800

    【i】【重构前移除：代码生成器】

--- engine-be-v1/be-generate/src/main/java/com/cmcorg20230301/engine/be/generate/model/constant/WebModelConstant.java
+++
@@ -1,8 +0,0 @@
-package com.cmcorg20230301.engine.be.generate.model.constant;
-
-public interface WebModelConstant {
-
-    String TABLE_IGNORE_FIELDS_ONE = "createId,createTime,updateId,updateTime,remark";
-    String TABLE_IGNORE_FIELDS_TWO = "createId,updateId";
-
-}
