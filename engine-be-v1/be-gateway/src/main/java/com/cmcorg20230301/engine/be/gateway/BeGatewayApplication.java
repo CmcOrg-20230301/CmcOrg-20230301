740cb5806a89cca5fdc25adba2d4231eb417eb98
caoguiming <1677664380@qq.com>
2023-03-22 11:27:52 +0800

    【i】【代码优化】

--- engine-be-v1/be-gateway/src/main/java/com/cmcorg20230301/engine/be/gateway/BeGatewayApplication.java
+++ engine-be-v1/be-gateway/src/main/java/com/cmcorg20230301/engine/be/gateway/BeGatewayApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class GatewayApplication {
+public class BeGatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GatewayApplication.class, args);
+        SpringApplication.run(BeGatewayApplication.class, args);
     }
 
 }
