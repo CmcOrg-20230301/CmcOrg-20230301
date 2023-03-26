348d0ff31c6daf03b6c6e6c6c4864edb51887ce3
法外狂徒李四 <dimensional_logic@qq.com>
2023-03-26 21:36:51 +0800

    【i】【手机号，登录注册相关接口测试工具】

---
+++ engine-be-v1/be-generate/src/main/java/com/cmcorg20230301/engine/be/generate/util/ApiTestSignPhoneUtil.java
@@ -0,0 +1,34 @@
+package com.cmcorg20230301.engine.be.generate.util;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 手机号，登录注册相关接口测试工具
+ */
+@Slf4j
+public class ApiTestSignPhoneUtil {
+
+    // 执行，接口的地址，备注：最后面不要加斜杠 /
+    //    private static final String API_ENDPOINT = "http://43.154.37.130:10001";
+    private static final String API_ENDPOINT = "http://127.0.0.1:10001";
+
+    // 手机号
+    private static final String PHONE = "12345";
+
+    // 新手机号
+    private static final String NEW_PHONE = "123456";
+
+    // 密码
+    private static final String PASSWORD_TEMP = "Ik123456";
+
+    // 新密码
+    private static final String NEW_PASSWORD_TEMP = "Ik1234567";
+
+    // 新密码-2
+    private static final String NEW_PASSWORD_2_TEMP = "Ik12345678";
+
+    public static void main(String[] args) {
+
+    }
+
+}
