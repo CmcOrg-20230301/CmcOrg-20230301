9e37a48f181c2d4cdd6b9adaa800c82adcd33b49
caoguiming <1677664380@qq.com>
2023-03-27 14:33:40 +0800

    【i】【手机号，登录注册相关接口测试工具】

--- engine-be-v1/be-generate/src/main/java/com/cmcorg20230301/engine/be/generate/util/ApiTestSignPhoneUtil.java
+++ engine-be-v1/be-generate/src/main/java/com/cmcorg20230301/engine/be/generate/util/ApiTestSignPhoneUtil.java
@@ -1,5 +1,9 @@
 package com.cmcorg20230301.engine.be.generate.util;
 
+import cn.hutool.http.HttpRequest;
+import cn.hutool.json.JSONUtil;
+import cn.hutool.setting.Setting;
+import com.cmcorg20230301.engine.be.sign.phone.model.dto.PhoneNotBlankDTO;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -12,11 +16,14 @@ public class ApiTestSignPhoneUtil {
     //    private static final String API_ENDPOINT = "http://43.154.37.130:10001";
     private static final String API_ENDPOINT = "http://127.0.0.1:10001";
 
+    // 配置文件
+    private static final Setting SETTING = new Setting("sign.setting");
+
     // 手机号
-    private static final String PHONE = "12345";
+    private static final String PHONE = SETTING.getStr("phone");
 
     // 新手机号
-    private static final String NEW_PHONE = "123456";
+    private static final String NEW_PHONE = SETTING.getStr("newPhone");
 
     // 密码
     private static final String PASSWORD_TEMP = "Ik123456";
@@ -28,6 +35,39 @@ public class ApiTestSignPhoneUtil {
     private static final String NEW_PASSWORD_2_TEMP = "Ik12345678";
 
     public static void main(String[] args) {
+
+        // 执行
+        exec(API_ENDPOINT, PHONE, PASSWORD_TEMP, NEW_PHONE, NEW_PASSWORD_TEMP, ApiTestHelper.RSA_PUBLIC_KEY,
+            NEW_PASSWORD_2_TEMP);
+
+    }
+
+    /**
+     * 执行
+     */
+    private static void exec(String apiEndpoint, String phone, String passwordTemp, String newPhone,
+        String newPasswordTemp, String rsaPublicKey, String newPassword2Temp) {
+
+        // 手机号-注册-发送验证码
+        phoneSignUpSendCode(apiEndpoint, phone);
+
+    }
+
+    /**
+     * 手机号-注册-发送验证码
+     */
+    private static void phoneSignUpSendCode(String apiEndpoint, String phone) {
+
+        long currentTs = System.currentTimeMillis();
+
+        PhoneNotBlankDTO dto = new PhoneNotBlankDTO();
+        dto.setPhone(phone);
+
+        String bodyStr =
+            HttpRequest.post(apiEndpoint + "/sign/phone/sign/up/sendCode").body(JSONUtil.toJsonStr(dto)).execute()
+                .body();
+
+        log.info("手机号-注册-发送验证码：耗时：{}，bodyStr：{}", ApiTestHelper.calcCostMs(currentTs), bodyStr);
 
     }
 
