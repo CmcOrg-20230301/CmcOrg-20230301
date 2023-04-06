754d11ea1ae91e5e12ab567827dd94b5b3945f77
caoguiming <1677664380@qq.com>
2023-04-06 18:27:56 +0800

    【i】【生成 api的工具类】

--- engine-be-v1/be-generate/src/main/java/com/cmcorg20230301/engine/be/generate/util/generate/SpringDocUtil.java
+++ engine-be-v1/be-generate/src/main/java/com/cmcorg20230301/engine/be/generate/util/generate/SpringDocUtil.java
@@ -24,8 +24,8 @@ import java.util.Map;
 public class SpringDocUtil {
 
     // 读取：接口的地址
-    //    private static final String SPRING_DOC_ENDPOINT = "http://43.154.37.130:10001/v3/api-docs/be";
-    private static final String SPRING_DOC_ENDPOINT = "http://127.0.0.1:10001/v3/api-docs/be";
+    private static final String SPRING_DOC_ENDPOINT = "http://43.154.37.130:10001/v3/api-docs/be";
+    //    private static final String SPRING_DOC_ENDPOINT = "http://127.0.0.1:10001/v3/api-docs/be";
 
     private static final String BE_API_SCHEMA_MAP_KEY = "beApiSchemaMapKey";
 
