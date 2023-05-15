96d17749eb39cfc6fd12770195b28ffb0db49773
caoguiming <1677664380@qq.com>
2023-05-15 18:17:00 +0800

    【i】【支付系统】

--- engine-be-v1/be-pay-wx/src/main/java/com/cmcorg20230301/engine/be/pay/wx/properties/PayWxProperties.java
+++ engine-be-v1/be-pay-wx/src/main/java/com/cmcorg20230301/engine/be/pay/wx/properties/PayWxProperties.java
@@ -5,14 +5,12 @@ import com.cmcorg20230301.engine.be.security.properties.SysPayBaseProperties;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
 @Component
 @ConfigurationProperties(prefix = PropertiesPrefixConstant.PAY_WX)
-@RefreshScope
 public class PayWxProperties extends SysPayBaseProperties {
 
     /**
