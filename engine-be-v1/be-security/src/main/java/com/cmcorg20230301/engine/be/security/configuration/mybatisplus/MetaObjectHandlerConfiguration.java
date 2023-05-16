c6e7f0a405eb6d8cef0ee35ac5fbbc67e4a22a83
caoguiming <1677664380@qq.com>
2023-05-16 11:15:28 +0800

    【i】【优化】【代码】

--- engine-be-v1/be-security/src/main/java/com/cmcorg20230301/engine/be/security/configuration/mybatisplus/MetaObjectHandlerConfiguration.java
+++ engine-be-v1/be-security/src/main/java/com/cmcorg20230301/engine/be/security/configuration/mybatisplus/MetaObjectHandlerConfiguration.java
@@ -1,7 +1,6 @@
 package com.cmcorg20230301.engine.be.security.configuration.mybatisplus;
 
 import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
-import com.cmcorg20230301.engine.be.model.model.constant.BaseConstant;
 import com.cmcorg20230301.engine.be.security.util.UserUtil;
 import org.apache.ibatis.reflection.MetaObject;
 import org.springframework.stereotype.Component;
@@ -31,7 +30,7 @@ public class MetaObjectHandlerConfiguration implements MetaObjectHandler {
 
         // 实体类有值时，这里不会生效
         this.strictUpdateFill(metaObject, "updateTime", Date.class, new Date());
-        this.strictUpdateFill(metaObject, "updateId", Long.class, BaseConstant.SYS_ID);
+        this.strictUpdateFill(metaObject, "updateId", Long.class, UserUtil.getCurrentUserIdDefault());
 
     }
 
