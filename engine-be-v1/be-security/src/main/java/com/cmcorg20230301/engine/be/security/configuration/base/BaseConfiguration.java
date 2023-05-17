eb606f103a69d9217e523c942251c46ffe9a4fd5
caoguiming <1677664380@qq.com>
2023-05-17 17:59:32 +0800

    【i】【优化】【代码】

--- engine-be-v1/be-security/src/main/java/com/cmcorg20230301/engine/be/security/configuration/base/BaseConfiguration.java
+++ engine-be-v1/be-security/src/main/java/com/cmcorg20230301/engine/be/security/configuration/base/BaseConfiguration.java
@@ -45,6 +45,15 @@ public class BaseConfiguration {
     }
 
     /**
+     * 获取：是否是开发环境
+     */
+    public static boolean devFlag() {
+
+        return "dev".equals(BaseConfiguration.profilesActive);
+
+    }
+
+    /**
      * 设置：@Async的线程池
      */
     @Bean
