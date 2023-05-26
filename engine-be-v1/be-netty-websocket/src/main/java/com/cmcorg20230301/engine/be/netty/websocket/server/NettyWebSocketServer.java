41e0bbea786a63d7c01971c5504e71a1b2c26ce6
dimlt <dimlt@vip.com>
2023-05-27 01:02:58 +0800

    【i】【优化】【NettyWebSocketServer.java】

--- engine-be-v1/be-netty-websocket/src/main/java/com/cmcorg20230301/engine/be/netty/websocket/server/NettyWebSocketServer.java
+++ engine-be-v1/be-netty-websocket/src/main/java/com/cmcorg20230301/engine/be/netty/websocket/server/NettyWebSocketServer.java
@@ -1,6 +1,9 @@
 package com.cmcorg20230301.engine.be.netty.websocket.server;
 
+import com.cmcorg20230301.engine.be.netty.websocket.configuration.NettyWebSocketBeanPostProcessor;
 import com.cmcorg20230301.engine.be.netty.websocket.properties.NettyWebSocketProperties;
+import com.cmcorg20230301.engine.be.security.configuration.base.BaseConfiguration;
+import com.cmcorg20230301.engine.be.security.util.MyThreadUtil;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
@@ -17,22 +20,20 @@ import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
-import javax.annotation.Resource;
 
 @Component
 @Slf4j
 public class NettyWebSocketServer {
 
-    @Resource
-    NettyWebSocketProperties nettyWebSocketProperties;
+    public NettyWebSocketServer(BaseConfiguration baseConfiguration, NettyWebSocketProperties nettyWebSocketProperties,
+        NettyWebSocketServerHandler nettyWebSocketServerHandler, MyThreadUtil myThreadUtil) {
 
-    @Resource
-    NettyWebSocketServerHandler nettyWebSocketServerHandler;
+        MyThreadUtil.execute(() -> {
 
-    @PostConstruct
-    public void postConstruct() {
+            start(nettyWebSocketProperties, nettyWebSocketServerHandler, BaseConfiguration.port + 1);
+
+        });
 
     }
 
@@ -42,7 +43,8 @@ public class NettyWebSocketServer {
     }
 
     @SneakyThrows
-    public void start(int port) {
+    public void start(NettyWebSocketProperties nettyWebSocketProperties,
+        NettyWebSocketServerHandler nettyWebSocketServerHandler, int port) {
 
         EventLoopGroup childGroup = new NioEventLoopGroup();
 
@@ -66,7 +68,6 @@ public class NettyWebSocketServer {
                     protected void initChannel(SocketChannel ch) { // 绑定客户端连接时候触发操作
 
                         // websocket协议本身是基于http协议的，所以这边也要使用http解编码器
-
                         ch.pipeline().addLast(new HttpServerCodec());
 
                         // 以块的方式来写的处理器
@@ -85,6 +86,8 @@ public class NettyWebSocketServer {
                 });
 
             ChannelFuture channelFuture = serverBootstrap.bind().sync(); // 服务器同步创建绑定
+
+            log.info("NettyWebSocket 启动完成，端口：{}，总接口个数：{}个", port, NettyWebSocketBeanPostProcessor.getMappingMapSize());
 
             channelFuture.channel().closeFuture().sync(); // 阻塞线程，监听关闭事件
 
