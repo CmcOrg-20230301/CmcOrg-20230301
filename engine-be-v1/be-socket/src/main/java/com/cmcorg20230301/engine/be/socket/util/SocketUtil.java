68295e99eff2c22af2f353c18852d4aedab573ad
dimlt <dimlt@vip.com>
2023-07-11 19:02:48 +0800

    【i】【优化】【websocket】

--- engine-be-v1/be-socket/src/main/java/com/cmcorg20230301/engine/be/socket/util/SocketUtil.java
+++ engine-be-v1/be-socket/src/main/java/com/cmcorg20230301/engine/be/socket/util/SocketUtil.java
@@ -1,6 +1,9 @@
 package com.cmcorg20230301.engine.be.socket.util;
 
+import cn.hutool.core.net.NetUtil;
+import com.cmcorg20230301.engine.be.security.util.RequestUtil;
 import io.netty.channel.Channel;
+import io.netty.handler.codec.http.FullHttpRequest;
 
 import java.net.InetSocketAddress;
 
@@ -14,6 +17,29 @@ public class SocketUtil {
         InetSocketAddress inetSocketAddress = (InetSocketAddress)channel.remoteAddress();
 
         return inetSocketAddress.getAddress().getHostAddress();
+
+    }
+
+    /**
+     * 获取：ip
+     */
+    public static String getIp(FullHttpRequest fullHttpRequest) {
+
+        String ip = "";
+
+        for (String item : RequestUtil.IP_HEADER_ARR) {
+
+            ip = fullHttpRequest.headers().get(item);
+
+            if (NetUtil.isUnknown(ip) == false) {
+
+                return NetUtil.getMultistageReverseProxyIp(ip);
+
+            }
+
+        }
+
+        return ip;
 
     }
 
