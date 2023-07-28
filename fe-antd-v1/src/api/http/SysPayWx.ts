2b25068d56ff4828a1fc0a63b250102997c65ad7
caoguiming <1677664380@qq.com>
2023-07-28 10:14:12 +0800

    【i】【优化】【api】

--- fe-antd-v1/src/api/http/SysPayWx.ts
+++ fe-antd-v1/src/api/http/SysPayWx.ts
@@ -1,6 +1,11 @@
 import $http from "@/util/HttpUtil";
 import {AxiosRequestConfig} from "axios";
 
+// 服务器异步通知-jsApi，备注：第三方应用调用
+export function SysPayWxNotifyCallBackJsApi(config?: AxiosRequestConfig) {
+    return $http.myPost<void>('/sys/payWx/notifyCallBack/jsApi', undefined, config)
+}
+
 // 服务器异步通知-native，备注：第三方应用调用
 export function SysPayWxNotifyCallBackNative(config?: AxiosRequestConfig) {
     return $http.myPost<void>('/sys/payWx/notifyCallBack/native', undefined, config)
