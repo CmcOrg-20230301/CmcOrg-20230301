9df58a693e768478b62958419e0a96a6dc0940bf
caoguiming <1677664380@qq.com>
2023-08-09 18:33:16 +0800

    【i】【优化】【socket】

--- fe-antd-v1/src/page/sys/SocketRefUser/TableColumnList.tsx
+++ fe-antd-v1/src/page/sys/SocketRefUser/TableColumnList.tsx
@@ -71,6 +71,7 @@ const TableColumnList = (actionRef: React.RefObject<ActionType | undefined>): Pr
         title: '操作',
         dataIndex: 'option',
         valueType: 'option',
+        width: 90,
 
         render: (dom, entity) => [
 
