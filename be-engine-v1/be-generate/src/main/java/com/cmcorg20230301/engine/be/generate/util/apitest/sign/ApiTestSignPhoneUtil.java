3e58c0b5ccd193c8b5b81d5a45defe25dea35205
caoguiming <1677664380@qq.com>
2023-08-24 16:29:15 +0800

    【i】【优化】【be-engine-v1】

---
+++ be-engine-v1/be-generate/src/main/java/com/cmcorg20230301/engine/be/generate/util/apitest/sign/ApiTestSignPhoneUtil.java
@@ -0,0 +1,380 @@
+package com.cmcorg20230301.engine.be.generate.util.apitest.sign;
+
+import cn.hutool.crypto.digest.DigestUtil;
+import cn.hutool.http.HttpRequest;
+import cn.hutool.json.JSONUtil;
+import cn.hutool.setting.Setting;
+import com.cmcorg20230301.engine.be.generate.util.apitest.ApiTestHelper;
+import com.cmcorg20230301.engine.be.model.model.dto.NotBlankCodeDTO;
+import com.cmcorg20230301.engine.be.security.util.MyRsaUtil;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 手机号，登录注册相关接口测试工具
+ */
+@Slf4j
+public class ApiTestSignPhoneUtil {
+
+    // 执行，接口的地址，备注：最后面不要加斜杠 /
+    //    private static final String API_ENDPOINT = "http://43.154.37.130:10001";
+    private static final String API_ENDPOINT = "http://127.0.0.1:10001";
+
+    // 配置文件
+    private static final Setting SETTING = new Setting("sign.setting");
+
+    // 手机号
+    public static final String PHONE = SETTING.getStr("phone");
+
+    // 新手机号
+    private static final String NEW_PHONE = SETTING.getStr("newPhone");
+
+    // 密码
+    private static final String PASSWORD_TEMP = "Ik123456";
+
+    // 新密码
+    private static final String NEW_PASSWORD_TEMP = "Ik1234567";
+
+    // 新密码-2
+    private static final String NEW_PASSWORD_2_TEMP = "Ik12345678";
+
+    public static void main(String[] args) {
+
+        // 执行
+        exec(API_ENDPOINT, PHONE, PASSWORD_TEMP, NEW_PHONE, NEW_PASSWORD_TEMP, ApiTestHelper.RSA_PUBLIC_KEY,
+            NEW_PASSWORD_2_TEMP);
+
+    }
+
+    /**
+     * 执行
+     */
+    private static void exec(String apiEndpoint, String phone, String passwordTemp, String newPhone,
+        String newPasswordTemp, String rsaPublicKey, String newPassword2Temp) {
+
+        // 手机号-注册-发送验证码
+        phoneSignUpSendCode(apiEndpoint, phone);
+
+        String code = ApiTestHelper.getStrFromScanner("请输入验证码");
+
+        // 手机号-注册
+        phoneSignUp(apiEndpoint, phone, passwordTemp, rsaPublicKey, code);
+
+        // 手机号-账号密码登录
+        String jwt = phoneSignInPassword(apiEndpoint, phone, passwordTemp, rsaPublicKey);
+
+        // 手机号-修改密码-发送验证码
+        phoneUpdatePasswordSendCode(apiEndpoint, jwt);
+
+        code = ApiTestHelper.getStrFromScanner("请输入验证码");
+
+        // 手机号-修改密码
+        phoneUpdatePassword(apiEndpoint, jwt, code, newPasswordTemp, rsaPublicKey);
+
+        // 手机号-账号密码登录
+        jwt = phoneSignInPassword(apiEndpoint, phone, newPasswordTemp, rsaPublicKey);
+
+        // 手机号-修改手机-发送验证码
+        phoneUpdateAccountSendCode(apiEndpoint, jwt);
+
+        // 手机号-注册-发送验证码
+        phoneSignUpSendCode(apiEndpoint, newPhone);
+
+        code = ApiTestHelper.getStrFromScanner("请输入旧手机验证码");
+
+        String newCode = ApiTestHelper.getStrFromScanner("请输入新手机验证码");
+
+        // 手机号-修改手机
+        phoneUpdateAccount(apiEndpoint, jwt, newPhone, code, newCode);
+
+        // 手机号-忘记密码-发送验证码
+        phoneForgetPasswordSendCode(apiEndpoint, newPhone);
+
+        code = ApiTestHelper.getStrFromScanner("请输入验证码");
+
+        // 手机号-忘记密码
+        phoneForgetPassword(apiEndpoint, newPhone, code, newPassword2Temp, rsaPublicKey);
+
+        //        // 手机号-账号密码登录
+        //        jwt = phoneSignInPassword(apiEndpoint, newPhone, newPassword2Temp, rsaPublicKey);
+
+        // 手机号-手机验证码登录-发送验证码
+        phoneSignInSendCode(apiEndpoint, newPhone);
+
+        code = ApiTestHelper.getStrFromScanner("请输入验证码");
+
+        // 手机号-手机验证码登录
+        jwt = phoneSignInCode(apiEndpoint, newPhone, code);
+
+        // 手机号-账号注销-发送验证码
+        phoneSignDeleteSendCode(apiEndpoint, jwt);
+
+        code = ApiTestHelper.getStrFromScanner("请输入验证码");
+
+        // 手机号-账号注销
+        phoneSignDelete(apiEndpoint, jwt, code);
+
+    }
+
+    /**
+     * 手机号-手机验证码登录
+     */
+    private static String phoneSignInCode(String apiEndpoint, String newPhone, String code) {
+
+        long currentTs = System.currentTimeMillis();
+
+        SignPhoneSignInCodeDTO dto = new SignPhoneSignInCodeDTO();
+        dto.setCode(code);
+        dto.setPhone(newPhone);
+
+        String bodyStr =
+            HttpRequest.post(apiEndpoint + "/sign/phone/sign/in/code").body(JSONUtil.toJsonStr(dto)).execute().body();
+
+        log.info("手机号-手机验证码登录：耗时：{}，bodyStr：{}", ApiTestHelper.calcCostMs(currentTs), bodyStr);
+
+        return JSONUtil.parseObj(bodyStr).getStr("data");
+
+    }
+
+    /**
+     * 手机号-手机验证码登录-发送验证码
+     */
+    private static void phoneSignInSendCode(String apiEndpoint, String newPhone) {
+
+        long currentTs = System.currentTimeMillis();
+
+        PhoneNotBlankDTO dto = new PhoneNotBlankDTO();
+        dto.setPhone(newPhone);
+
+        String bodyStr =
+            HttpRequest.post(apiEndpoint + "/sign/phone/sign/in/sendCode").body(JSONUtil.toJsonStr(dto)).execute()
+                .body();
+
+        log.info("手机号-手机验证码登录-发送验证码：耗时：{}，bodyStr：{}", ApiTestHelper.calcCostMs(currentTs), bodyStr);
+
+    }
+
+    /**
+     * 手机号-账号注销
+     */
+    private static void phoneSignDelete(String apiEndpoint, String jwt, String code) {
+
+        long currentTs = System.currentTimeMillis();
+
+        NotBlankCodeDTO dto = new NotBlankCodeDTO();
+        dto.setCode(code);
+
+        String bodyStr = HttpRequest.post(apiEndpoint + "/sign/phone/signDelete").header("Authorization", jwt)
+            .body(JSONUtil.toJsonStr(dto)).execute().body();
+
+        log.info("手机号-账号注销：耗时：{}，bodyStr：{}", ApiTestHelper.calcCostMs(currentTs), bodyStr);
+
+    }
+
+    /**
+     * 手机号-账号注销-发送验证码
+     */
+    private static void phoneSignDeleteSendCode(String apiEndpoint, String jwt) {
+
+        long currentTs = System.currentTimeMillis();
+
+        String bodyStr =
+            HttpRequest.post(apiEndpoint + "/sign/phone/signDelete/sendCode").header("Authorization", jwt).execute()
+                .body();
+
+        log.info("手机号-账号注销-发送验证码：耗时：{}，bodyStr：{}", ApiTestHelper.calcCostMs(currentTs), bodyStr);
+
+    }
+
+    /**
+     * 手机号-忘记密码
+     */
+    private static void phoneForgetPassword(String apiEndpoint, String newPhone, String code, String newPassword2Temp,
+        String rsaPublicKey) {
+
+        long currentTs = System.currentTimeMillis();
+
+        String originPassword = MyRsaUtil.rsaEncrypt(newPassword2Temp, rsaPublicKey);
+
+        String password = DigestUtil.sha256Hex((DigestUtil.sha512Hex(newPassword2Temp)));
+
+        password = MyRsaUtil.rsaEncrypt(password, rsaPublicKey);
+
+        SignPhoneForgetPasswordDTO dto = new SignPhoneForgetPasswordDTO();
+        dto.setCode(code);
+        dto.setNewPassword(password);
+        dto.setOriginNewPassword(originPassword);
+        dto.setPhone(newPhone);
+
+        String bodyStr =
+            HttpRequest.post(apiEndpoint + "/sign/phone/forgetPassword").body(JSONUtil.toJsonStr(dto)).execute().body();
+
+        log.info("手机号-忘记密码：耗时：{}，bodyStr：{}", ApiTestHelper.calcCostMs(currentTs), bodyStr);
+
+    }
+
+    /**
+     * 手机号-忘记密码-发送验证码
+     */
+    private static void phoneForgetPasswordSendCode(String apiEndpoint, String newPhone) {
+
+        long currentTs = System.currentTimeMillis();
+
+        PhoneNotBlankDTO dto = new PhoneNotBlankDTO();
+        dto.setPhone(newPhone);
+
+        String bodyStr =
+            HttpRequest.post(apiEndpoint + "/sign/phone/forgetPassword/sendCode").body(JSONUtil.toJsonStr(dto))
+                .execute().body();
+
+        log.info("手机号-忘记密码-发送验证码：耗时：{}，bodyStr：{}", ApiTestHelper.calcCostMs(currentTs), bodyStr);
+
+    }
+
+    /**
+     * 手机号-修改手机
+     */
+    private static void phoneUpdateAccount(String apiEndpoint, String jwt, String newPhone, String code,
+        String newCode) {
+
+        long currentTs = System.currentTimeMillis();
+
+        SignPhoneUpdateAccountDTO dto = new SignPhoneUpdateAccountDTO();
+        dto.setNewPhone(newPhone);
+        dto.setOldPhoneCode(code);
+        dto.setNewPhoneCode(newCode);
+
+        String bodyStr = HttpRequest.post(apiEndpoint + "/sign/phone/updateAccount").body(JSONUtil.toJsonStr(dto))
+            .header("Authorization", jwt).execute().body();
+
+        log.info("手机号-修改手机：耗时：{}，bodyStr：{}", ApiTestHelper.calcCostMs(currentTs), bodyStr);
+
+    }
+
+    /**
+     * 手机号-修改手机-发送验证码
+     */
+    private static void phoneUpdateAccountSendCode(String apiEndpoint, String jwt) {
+
+        long currentTs = System.currentTimeMillis();
+
+        String bodyStr =
+            HttpRequest.post(apiEndpoint + "/sign/phone/updateAccount/sendCode").header("Authorization", jwt).execute()
+                .body();
+
+        log.info("手机号-修改手机-发送验证码：耗时：{}，bodyStr：{}", ApiTestHelper.calcCostMs(currentTs), bodyStr);
+
+    }
+
+    /**
+     * 手机号-修改密码
+     */
+    private static void phoneUpdatePassword(String apiEndpoint, String jwt, String code, String newPasswordTemp,
+        String rsaPublicKey) {
+
+        long currentTs = System.currentTimeMillis();
+
+        String originNewPassword = MyRsaUtil.rsaEncrypt(newPasswordTemp, rsaPublicKey);
+
+        String newPassword = DigestUtil.sha256Hex((DigestUtil.sha512Hex(newPasswordTemp)));
+
+        newPassword = MyRsaUtil.rsaEncrypt(newPassword, rsaPublicKey);
+
+        SignPhoneUpdatePasswordDTO dto = new SignPhoneUpdatePasswordDTO();
+        dto.setCode(code);
+        dto.setNewPassword(newPassword);
+        dto.setOriginNewPassword(originNewPassword);
+
+        String bodyStr = HttpRequest.post(apiEndpoint + "/sign/phone/updatePassword").body(JSONUtil.toJsonStr(dto))
+            .header("Authorization", jwt).execute().body();
+
+        log.info("手机号-修改密码：耗时：{}，bodyStr：{}", ApiTestHelper.calcCostMs(currentTs), bodyStr);
+
+    }
+
+    /**
+     * 手机号-修改密码-发送验证码
+     */
+    private static void phoneUpdatePasswordSendCode(String apiEndpoint, String jwt) {
+
+        long currentTs = System.currentTimeMillis();
+
+        String bodyStr =
+            HttpRequest.post(apiEndpoint + "/sign/phone/updatePassword/sendCode").header("Authorization", jwt).execute()
+                .body();
+
+        log.info("手机号-修改密码-发送验证码：耗时：{}，bodyStr：{}", ApiTestHelper.calcCostMs(currentTs), bodyStr);
+
+    }
+
+    /**
+     * 手机号-账号密码登录
+     */
+    private static String phoneSignInPassword(String apiEndpoint, String phone, String passwordTemp,
+        String rsaPublicKey) {
+
+        long currentTs = System.currentTimeMillis();
+
+        String password = DigestUtil.sha256Hex((DigestUtil.sha512Hex(passwordTemp)));
+
+        password = MyRsaUtil.rsaEncrypt(password, rsaPublicKey);
+
+        SignPhoneSignInPasswordDTO dto = new SignPhoneSignInPasswordDTO();
+        dto.setPassword(password);
+        dto.setPhone(phone);
+
+        String bodyStr =
+            HttpRequest.post(apiEndpoint + "/sign/phone/sign/in/password").body(JSONUtil.toJsonStr(dto)).execute()
+                .body();
+
+        log.info("手机号-账号密码登录：耗时：{}，bodyStr：{}", ApiTestHelper.calcCostMs(currentTs), bodyStr);
+
+        return JSONUtil.parseObj(bodyStr).getStr("data");
+
+    }
+
+    /**
+     * 手机号-注册
+     */
+    private static void phoneSignUp(String apiEndpoint, String phone, String passwordTemp, String rsaPublicKey,
+        String code) {
+
+        long currentTs = System.currentTimeMillis();
+
+        String originPassword = MyRsaUtil.rsaEncrypt(passwordTemp, rsaPublicKey);
+
+        String password = DigestUtil.sha256Hex((DigestUtil.sha512Hex(passwordTemp)));
+
+        password = MyRsaUtil.rsaEncrypt(password, rsaPublicKey);
+
+        SignPhoneSignUpDTO dto = new SignPhoneSignUpDTO();
+        dto.setCode(code);
+        dto.setPassword(password);
+        dto.setOriginPassword(originPassword);
+        dto.setPhone(phone);
+
+        String bodyStr =
+            HttpRequest.post(apiEndpoint + "/sign/phone/sign/up").body(JSONUtil.toJsonStr(dto)).execute().body();
+
+        log.info("手机号-注册：耗时：{}，bodyStr：{}", ApiTestHelper.calcCostMs(currentTs), bodyStr);
+
+    }
+
+    /**
+     * 手机号-注册-发送验证码
+     */
+    private static void phoneSignUpSendCode(String apiEndpoint, String phone) {
+
+        long currentTs = System.currentTimeMillis();
+
+        PhoneNotBlankDTO dto = new PhoneNotBlankDTO();
+        dto.setPhone(phone);
+
+        String bodyStr =
+            HttpRequest.post(apiEndpoint + "/sign/phone/sign/up/sendCode").body(JSONUtil.toJsonStr(dto)).execute()
+                .body();
+
+        log.info("手机号-注册-发送验证码：耗时：{}，bodyStr：{}", ApiTestHelper.calcCostMs(currentTs), bodyStr);
+
+    }
+
+}
---
+++ be-engine-v1/be-generate/src/main/java/com/cmcorg20230301/engine/be/generate/util/apitest/sign/ApiTestSignSignInNameUtil.java
@@ -0,0 +1,306 @@
+package com.cmcorg20230301.engine.be.generate.util.apitest.sign;
+
+import cn.hutool.crypto.digest.DigestUtil;
+import cn.hutool.http.HttpRequest;
+import cn.hutool.json.JSONUtil;
+import cn.hutool.setting.Setting;
+import com.cmcorg20230301.engine.be.generate.util.apitest.ApiTestHelper;
+import com.cmcorg20230301.engine.be.generate.util.apitest.sys.ApiTestSysMenuUtil;
+import com.cmcorg20230301.engine.be.security.util.MyRsaUtil;
+import com.cmcorg20230301.engine.be.sign.email.model.dto.EmailNotBlankDTO;
+import com.cmcorg20230301.engine.be.sign.email.model.dto.SignEmailBindAccountDTO;
+import com.cmcorg20230301.engine.be.sign.phone.model.dto.PhoneNotBlankDTO;
+import com.cmcorg20230301.engine.be.sign.phone.model.dto.SignPhoneBindAccountDTO;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 登录名，登录注册相关接口测试工具
+ */
+@Slf4j
+public class ApiTestSignSignInNameUtil {
+
+    // 执行，接口的地址，备注：最后面不要加斜杠 /
+    //    private static final String API_ENDPOINT = "http://43.154.37.130:10001";
+    private static final String API_ENDPOINT = "http://127.0.0.1:10001";
+
+    // 配置文件
+    private static final Setting SETTING = new Setting("sign.setting");
+
+    // 登录名
+    private static final String SIGN_IN_NAME = "cxk";
+
+    // 新登录名
+    private static final String NEW_SIGN_IN_NAME = "cxk2";
+
+    // 密码
+    private static final String PASSWORD_TEMP = "Ik123456";
+
+    // 新密码
+    private static final String NEW_PASSWORD_TEMP = "Ik1234567";
+
+    // 邮箱
+    private static final String EMAIL = SETTING.getStr("email");
+
+    // 手机号
+    public static final String PHONE = SETTING.getStr("phone");
+
+    public static void main(String[] args) {
+
+        // 执行
+        exec(API_ENDPOINT, SIGN_IN_NAME, PASSWORD_TEMP, NEW_SIGN_IN_NAME, NEW_PASSWORD_TEMP,
+            ApiTestHelper.RSA_PUBLIC_KEY, EMAIL, PHONE);
+
+    }
+
+    /**
+     * 执行
+     */
+    private static void exec(String apiEndpoint, String signInName, String passwordTemp, String newSignInName,
+        String newPasswordTemp, String rsaPublicKey, String email, String phone) {
+
+        // 登录名-注册
+        signInNameSignUp(apiEndpoint, signInName, passwordTemp, rsaPublicKey);
+
+        // 登录名-用户名账号密码登录
+        String jwt = signInNameSignIn(apiEndpoint, signInName, passwordTemp, rsaPublicKey);
+
+        // 登录名-修改密码
+        signInNameUpdatePassword(apiEndpoint, jwt, passwordTemp, newPasswordTemp, rsaPublicKey);
+
+        // 登录名-用户名账号密码登录
+        jwt = signInNameSignIn(apiEndpoint, signInName, newPasswordTemp, rsaPublicKey);
+
+        // 登录名-修改账号
+        signInNameUpdateAccount(apiEndpoint, jwt, newSignInName, newPasswordTemp, rsaPublicKey);
+
+        // 登录名-用户名账号密码登录
+        jwt = signInNameSignIn(apiEndpoint, newSignInName, newPasswordTemp, rsaPublicKey);
+
+        // 绑定邮箱-发送验证码
+        emailBindAccountSendCode(apiEndpoint, jwt, email);
+
+        String code = ApiTestHelper.getStrFromScanner("请输入验证码");
+
+        // 绑定邮箱
+        emailBindAccount(apiEndpoint, jwt, email, code);
+
+        // 登录名-用户名账号密码登录
+        jwt = signInNameSignIn(apiEndpoint, newSignInName, newPasswordTemp, rsaPublicKey);
+
+        // 绑定手机号-发送验证码
+        phoneBindAccountSendCode(apiEndpoint, jwt, phone);
+
+        code = ApiTestHelper.getStrFromScanner("请输入验证码");
+
+        // 绑定手机号
+        phoneBindAccount(apiEndpoint, jwt, phone, code);
+
+        // 登录名-用户名账号密码登录
+        jwt = signInNameSignIn(apiEndpoint, newSignInName, newPasswordTemp, rsaPublicKey);
+
+        // 菜单-获取：当前用户绑定的菜单
+        ApiTestSysMenuUtil.sysMenuUserSelfMenuList(apiEndpoint, jwt);
+
+        // 登录名-账号注销
+        signInNameSignDelete(apiEndpoint, newPasswordTemp, jwt, rsaPublicKey);
+
+    }
+
+    /**
+     * 绑定手机号
+     */
+    private static void phoneBindAccount(String apiEndpoint, String jwt, String phone, String code) {
+
+        long currentTs = System.currentTimeMillis();
+
+        SignPhoneBindAccountDTO dto = new SignPhoneBindAccountDTO();
+        dto.setCode(code);
+        dto.setPhone(phone);
+
+        String bodyStr = HttpRequest.post(apiEndpoint + "/sign/phone/bindAccount").header("Authorization", jwt)
+            .body(JSONUtil.toJsonStr(dto)).execute().body();
+
+        log.info("绑定手机号：耗时：{}，bodyStr：{}", ApiTestHelper.calcCostMs(currentTs), bodyStr);
+
+    }
+
+    /**
+     * 绑定手机号-发送验证码
+     */
+    private static void phoneBindAccountSendCode(String apiEndpoint, String jwt, String phone) {
+
+        long currentTs = System.currentTimeMillis();
+
+        PhoneNotBlankDTO dto = new PhoneNotBlankDTO();
+        dto.setPhone(phone);
+
+        String bodyStr = HttpRequest.post(apiEndpoint + "/sign/phone/bindAccount/sendCode").header("Authorization", jwt)
+            .body(JSONUtil.toJsonStr(dto)).execute().body();
+
+        log.info("绑定手机号-发送验证码：耗时：{}，bodyStr：{}", ApiTestHelper.calcCostMs(currentTs), bodyStr);
+
+    }
+
+    /**
+     * 绑定邮箱
+     */
+    private static void emailBindAccount(String apiEndpoint, String jwt, String email, String code) {
+
+        long currentTs = System.currentTimeMillis();
+
+        SignEmailBindAccountDTO dto = new SignEmailBindAccountDTO();
+        dto.setCode(code);
+        dto.setEmail(email);
+
+        String bodyStr = HttpRequest.post(apiEndpoint + "/sign/email/bindAccount").header("Authorization", jwt)
+            .body(JSONUtil.toJsonStr(dto)).execute().body();
+
+        log.info("绑定邮箱：耗时：{}，bodyStr：{}", ApiTestHelper.calcCostMs(currentTs), bodyStr);
+
+    }
+
+    /**
+     * 绑定邮箱-发送验证码
+     */
+    private static void emailBindAccountSendCode(String apiEndpoint, String jwt, String email) {
+
+        long currentTs = System.currentTimeMillis();
+
+        EmailNotBlankDTO dto = new EmailNotBlankDTO();
+        dto.setEmail(email);
+
+        String bodyStr = HttpRequest.post(apiEndpoint + "/sign/email/bindAccount/sendCode").header("Authorization", jwt)
+            .body(JSONUtil.toJsonStr(dto)).execute().body();
+
+        log.info("绑定邮箱-发送验证码：耗时：{}，bodyStr：{}", ApiTestHelper.calcCostMs(currentTs), bodyStr);
+
+    }
+
+    /**
+     * 登录名-账号注销
+     */
+    private static void signInNameSignDelete(String apiEndpoint, String currentPasswordTemp, String jwt,
+        String rsaPublicKey) {
+
+        long currentTs = System.currentTimeMillis();
+
+        String currentPassword = DigestUtil.sha256Hex((DigestUtil.sha512Hex(currentPasswordTemp)));
+
+        currentPassword = MyRsaUtil.rsaEncrypt(currentPassword, rsaPublicKey);
+
+        SignSignInNameSignDeleteDTO dto = new SignSignInNameSignDeleteDTO();
+        dto.setCurrentPassword(currentPassword);
+
+        String bodyStr = HttpRequest.post(apiEndpoint + "/sign/signInName/signDelete").body(JSONUtil.toJsonStr(dto))
+            .header("Authorization", jwt).execute().body();
+
+        log.info("登录名-账号注销：耗时：{}，bodyStr：{}", ApiTestHelper.calcCostMs(currentTs), bodyStr);
+
+    }
+
+    /**
+     * 登录名-修改账号
+     */
+    private static void signInNameUpdateAccount(String apiEndpoint, String jwt, String newSignInName,
+        String currentPasswordTemp, String rsaPublicKey) {
+
+        long currentTs = System.currentTimeMillis();
+
+        String currentPassword = DigestUtil.sha256Hex((DigestUtil.sha512Hex(currentPasswordTemp)));
+
+        currentPassword = MyRsaUtil.rsaEncrypt(currentPassword, rsaPublicKey);
+
+        SignSignInNameUpdateAccountDTO dto = new SignSignInNameUpdateAccountDTO();
+        dto.setNewSignInName(newSignInName);
+        dto.setCurrentPassword(currentPassword);
+
+        String bodyStr = HttpRequest.post(apiEndpoint + "/sign/signInName/updateAccount").body(JSONUtil.toJsonStr(dto))
+            .header("Authorization", jwt).execute().body();
+
+        log.info("登录名-修改账号：耗时：{}，bodyStr：{}", ApiTestHelper.calcCostMs(currentTs), bodyStr);
+
+    }
+
+    /**
+     * 登录名-修改密码
+     */
+    private static void signInNameUpdatePassword(String apiEndpoint, String jwt, String passwordTemp,
+        String newPasswordTemp, String rsaPublicKey) {
+
+        long currentTs = System.currentTimeMillis();
+
+        String oldPassword = DigestUtil.sha256Hex((DigestUtil.sha512Hex(passwordTemp)));
+
+        oldPassword = MyRsaUtil.rsaEncrypt(oldPassword, rsaPublicKey);
+
+        String originNewPassword = MyRsaUtil.rsaEncrypt(newPasswordTemp, rsaPublicKey);
+
+        String newPassword = DigestUtil.sha256Hex((DigestUtil.sha512Hex(newPasswordTemp)));
+
+        newPassword = MyRsaUtil.rsaEncrypt(newPassword, rsaPublicKey);
+
+        SignSignInNameUpdatePasswordDTO dto = new SignSignInNameUpdatePasswordDTO();
+        dto.setOldPassword(oldPassword);
+        dto.setNewPassword(newPassword);
+        dto.setOriginNewPassword(originNewPassword);
+
+        String bodyStr = HttpRequest.post(apiEndpoint + "/sign/signInName/updatePassword").body(JSONUtil.toJsonStr(dto))
+            .header("Authorization", jwt).execute().body();
+
+        log.info("登录名-修改密码：耗时：{}，bodyStr：{}", ApiTestHelper.calcCostMs(currentTs), bodyStr);
+
+    }
+
+    /**
+     * 登录名-用户名账号密码登录
+     */
+    public static String signInNameSignIn(String apiEndpoint, String signInName, String passwordTemp,
+        String rsaPublicKey) {
+
+        long currentTs = System.currentTimeMillis();
+
+        String password = DigestUtil.sha256Hex((DigestUtil.sha512Hex(passwordTemp)));
+
+        password = MyRsaUtil.rsaEncrypt(password, rsaPublicKey);
+
+        SignSignInNameSignInPasswordDTO dto = new SignSignInNameSignInPasswordDTO();
+        dto.setPassword(password);
+        dto.setSignInName(signInName);
+
+        String bodyStr =
+            HttpRequest.post(apiEndpoint + "/sign/signInName/sign/in/password").body(JSONUtil.toJsonStr(dto)).execute()
+                .body();
+
+        log.info("登录名-用户名账号密码登录：耗时：{}，bodyStr：{}", ApiTestHelper.calcCostMs(currentTs), bodyStr);
+
+        return JSONUtil.parseObj(bodyStr).getStr("data");
+
+    }
+
+    /**
+     * 登录名-注册
+     */
+    private static void signInNameSignUp(String apiEndpoint, String signInName, String passwordTemp,
+        String rsaPublicKey) {
+
+        long currentTs = System.currentTimeMillis();
+
+        String originPassword = MyRsaUtil.rsaEncrypt(passwordTemp, rsaPublicKey);
+
+        String password = DigestUtil.sha256Hex((DigestUtil.sha512Hex(passwordTemp)));
+
+        password = MyRsaUtil.rsaEncrypt(password, rsaPublicKey);
+
+        SignSignInNameSignUpDTO dto = new SignSignInNameSignUpDTO();
+        dto.setPassword(password);
+        dto.setOriginPassword(originPassword);
+        dto.setSignInName(signInName);
+
+        String bodyStr =
+            HttpRequest.post(apiEndpoint + "/sign/signInName/sign/up").body(JSONUtil.toJsonStr(dto)).execute().body();
+
+        log.info("登录名-注册：耗时：{}，bodyStr：{}", ApiTestHelper.calcCostMs(currentTs), bodyStr);
+
+    }
+
+}
