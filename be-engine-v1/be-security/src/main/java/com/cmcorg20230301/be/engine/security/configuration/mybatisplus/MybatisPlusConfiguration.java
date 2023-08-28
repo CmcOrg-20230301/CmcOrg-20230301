8e67054bea9b9560dd486bd81068b67a1e541c09
caoguiming <1677664380@qq.com>
2023-08-28 18:09:06 +0800

    【i】【优化】【租户管理】

--- be-engine-v1/be-security/src/main/java/com/cmcorg20230301/be/engine/security/configuration/mybatisplus/MybatisPlusConfiguration.java
+++ be-engine-v1/be-security/src/main/java/com/cmcorg20230301/be/engine/security/configuration/mybatisplus/MybatisPlusConfiguration.java
@@ -3,9 +3,14 @@ package com.cmcorg20230301.be.engine.security.configuration.mybatisplus;
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.core.incrementer.IdentifierGenerator;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.handler.TenantLineHandler;
 import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.TenantLineInnerInterceptor;
 import com.cmcorg20230301.be.engine.redisson.util.IdGeneratorUtil;
+import com.cmcorg20230301.be.engine.security.util.UserUtil;
+import net.sf.jsqlparser.expression.Expression;
+import net.sf.jsqlparser.expression.LongValue;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -21,8 +26,19 @@ public class MybatisPlusConfiguration {
 
         MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
 
+        // 多租户
+        mybatisPlusInterceptor.addInnerInterceptor(new TenantLineInnerInterceptor(new TenantLineHandler() {
+
+            @Override
+            public Expression getTenantId() {
+                return new LongValue(UserUtil.getCurrentTenantIdDefault());
+            }
+
+        }));
+
         // 分页插件
         mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+
         // 乐观锁
         mybatisPlusInterceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
 
