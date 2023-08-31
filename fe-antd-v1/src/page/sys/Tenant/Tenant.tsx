68da489546416fdb6bc8c0d253ce64dc462567ed
caoguiming <1677664380@qq.com>
2023-08-31 15:24:28 +0800

    【i】【优化】【租户管理】

--- fe-antd-v1/src/page/sys/Tenant/Tenant.tsx
+++ fe-antd-v1/src/page/sys/Tenant/Tenant.tsx
@@ -62,8 +62,6 @@ export default function () {
 
                 actionRef={actionRef}
 
-                formRef={tableFormRef}
-
                 rowKey={"id"}
                 pagination={false}
                 columnEmptyText={false}
