f549f3ccd5a651cc24f36e2108067247fd5e61ed
caoguiming <1677664380@qq.com>
2023-09-13 17:53:34 +0800

    【i】【优化】【TransactionUtil.java】

--- be-engine-v1/be-datasource/src/main/java/com/cmcorg20230301/be/engine/datasource/util/TransactionUtil.java
+++ be-engine-v1/be-datasource/src/main/java/com/cmcorg20230301/be/engine/datasource/util/TransactionUtil.java
@@ -1,27 +1,14 @@
 package com.cmcorg20230301.be.engine.datasource.util;
 
 import cn.hutool.core.lang.func.VoidFunc0;
+import com.baomidou.dynamic.datasource.tx.LocalTxUtil;
 import lombok.SneakyThrows;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.TransactionDefinition;
-import org.springframework.transaction.TransactionStatus;
 
 import java.util.function.Supplier;
 
 @Component
 public class TransactionUtil {
-
-    private static DataSourceTransactionManager dataSourceTransactionManager;
-    private static TransactionDefinition transactionDefinition;
-
-    public TransactionUtil(DataSourceTransactionManager dataSourceTransactionManager,
-        TransactionDefinition transactionDefinition) {
-
-        TransactionUtil.dataSourceTransactionManager = dataSourceTransactionManager;
-        TransactionUtil.transactionDefinition = transactionDefinition;
-
-    }
 
     /**
      * 携带事务，执行方法
@@ -30,17 +17,18 @@ public class TransactionUtil {
     public static void exec(VoidFunc0 voidFunc0) {
 
         // 开启事务
-        TransactionStatus transactionStatus = dataSourceTransactionManager.getTransaction(transactionDefinition);
+        String xid = LocalTxUtil.startTransaction();
 
         try {
 
             voidFunc0.call();
 
-            dataSourceTransactionManager.commit(transactionStatus); // 提交
+            LocalTxUtil.commit(xid); // 提交
 
         } catch (Exception e) {
 
-            dataSourceTransactionManager.rollback(transactionStatus); // 回滚
+            LocalTxUtil.rollback(xid); // 回滚
+
             throw e;
 
         }
@@ -50,22 +38,24 @@ public class TransactionUtil {
     /**
      * 携带事务，执行方法
      */
+    @SneakyThrows
     public static <T> T exec(Supplier<T> supplier) {
 
         // 开启事务
-        TransactionStatus transactionStatus = dataSourceTransactionManager.getTransaction(transactionDefinition);
+        String xid = LocalTxUtil.startTransaction();
 
         try {
 
             T resObject = supplier.get();
 
-            dataSourceTransactionManager.commit(transactionStatus); // 提交
+            LocalTxUtil.commit(xid); // 提交
 
             return resObject;
 
         } catch (Exception e) {
 
-            dataSourceTransactionManager.rollback(transactionStatus); // 回滚
+            LocalTxUtil.rollback(xid); // 回滚
+
             throw e;
 
         }
