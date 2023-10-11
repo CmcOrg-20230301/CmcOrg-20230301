fd59ae9f54b637f31f3972650af4d284f698ba3b
caoguiming <1677664380@qq.com>
2023-10-11 16:07:32 +0800

    【i】【优化】【用户钱包】

--- be-engine-v1/be-wallet/src/main/java/com/cmcorg20230301/be/engine/wallet/service/SysTenantWalletService.java
+++ be-engine-v1/be-wallet/src/main/java/com/cmcorg20230301/be/engine/wallet/service/SysTenantWalletService.java
@@ -6,11 +6,6 @@ import com.cmcorg20230301.be.engine.model.model.dto.NotEmptyIdSet;
 import com.cmcorg20230301.be.engine.model.model.dto.NotNullId;
 import com.cmcorg20230301.be.engine.wallet.model.dto.SysUserWalletPageDTO;
 import com.cmcorg20230301.be.engine.wallet.model.entity.SysUserWalletDO;
-import com.cmcorg20230301.be.engine.wallet.model.enums.SysUserWalletLogTypeEnum;
-
-import java.math.BigDecimal;
-import java.util.Date;
-import java.util.Set;
 
 public interface SysTenantWalletService {
 
@@ -23,8 +18,5 @@ public interface SysTenantWalletService {
     SysUserWalletDO infoById(NotNullId notNullId);
 
     String addWithdrawableMoneyBackground(ChangeBigDecimalNumberDTO dto);
-
-    String doAddWithdrawableMoney(Long currentUserId, Date date, Set<Long> idSet, BigDecimal changeNumber,
-        SysUserWalletLogTypeEnum sysUserWalletLogTypeEnum, boolean lowErrorFlag, boolean checkWalletEnableFlag);
 
 }
