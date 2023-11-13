0b6e249bdcfbfde34fbfc3babd48a6b099a033e3
caoguiming <1677664380@qq.com>
2023-11-13 09:49:36 +0800

    【i】【优化】【代码】

--- be-engine-v1/be-pay/src/main/java/com/cmcorg20230301/be/engine/pay/base/model/vo/BuyVO.java
+++ be-engine-v1/be-pay/src/main/java/com/cmcorg20230301/be/engine/pay/base/model/vo/BuyVO.java
@@ -1,6 +1,5 @@
 package com.cmcorg20230301.be.engine.pay.base.model.vo;
 
-import com.cmcorg20230301.be.engine.pay.base.model.enums.SysPayTypeEnum;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -12,7 +11,7 @@ import lombok.NoArgsConstructor;
 public class BuyVO {
 
     @Schema(description = "实际的支付方式")
-    private SysPayTypeEnum sysPayTypeEnum;
+    private Integer sysPayType;
 
     @Schema(description = "支付返回的参数")
     private String payReturnValue;
