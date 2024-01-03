38263fcce4a7bd8b1fc3f62032cbdc492a74d050
caoguiming <1677664380@qq.com>
2024-01-03 18:49:41 +0800

    【i】【优化】【代码】

--- be-engine-v1/be-model/src/main/java/com/cmcorg20230301/be/engine/model/model/dto/UserIdAndTenantIdDTO.java
+++ be-engine-v1/be-model/src/main/java/com/cmcorg20230301/be/engine/model/model/dto/UserIdAndTenantIdDTO.java
@@ -3,19 +3,12 @@ package com.cmcorg20230301.be.engine.model.model.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
-
 @Data
 public class UserIdAndTenantIdDTO {
 
-    @Min(0)
-    @NotNull
     @Schema(description = "用户主键 id")
     private Long userId;
 
-    @Min(0)
-    @NotNull
     @Schema(description = "租户主键 id")
     private Long tenantId;
 
