447efcfacfbca01c0adc2b1b99c3d812a5aec798
Cmc0 <dimlt@vip.qq.com>
2024-01-26 18:36:59 +0800

    【i】【优化】【统一登录】

---
+++ be-engine-v1/be-sign-single/src/main/java/com/cmcorg20230301/be/engine/sign/single/configuration/SignSingleSecurityPermitConfiguration.java
@@ -0,0 +1,14 @@
+package com.cmcorg20230301.be.engine.sign.single.configuration;
+
+import com.cmcorg20230301.be.engine.sign.helper.configuration.AbstractSignHelperSecurityPermitConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SignSingleSecurityPermitConfiguration extends AbstractSignHelperSecurityPermitConfiguration {
+
+    @Override
+    protected String getSignPreUri() {
+        return "single";
+    }
+
+}
